141263c982062e88beea6b4e06d69934d32a3f74
Guilherme Arthur <35711609-arthssg@users.noreply.replit.com>
2024-08-09 12:54:47 +0000

    Initial commit

---
+++ tcp/client.go
@@ -0,0 +1,36 @@
+package main
+
+import "net"
+import "fmt"
+import "bufio"
+import "os"
+
+func main() {
+
+  conexao, erro1 := net.Dial("tcp", "127.0.0.1:8081")
+  if erro1 != nil {
+    fmt.Println(erro1)
+    os.Exit(3)
+  }
+  defer conexao.Close()
+  
+  for {
+    leitor := bufio.NewReader(os.Stdin)
+    fmt.Print("texto a ser enviado: ")
+    texto, erro2 := leitor.ReadString('\n')
+    if erro2 != nil {
+      fmt.Println(erro2)
+      os.Exit(3)
+    }
+
+    fmt.Fprintf(conexao, texto+"\n")
+
+    mensagem, erro3 := bufio.NewReader(conexao).ReadString('\n')
+    if erro3 != nil {
+      fmt.Println(erro3)
+      os.Exit(3)
+    }
+    
+    fmt.Print("Resposta do servidor: " + mensagem)
+  }
+}
---
+++ tcp/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "net"
+  "os"
+  "strings"
+)
+
+func main() {
+
+  fmt.Println("Servidor aguardando conexões...")
+
+  ln, erro1 := net.Listen("tcp", ":8081")
+  if erro1 != nil {
+    fmt.Println(erro1)
+    os.Exit(3)
+  }
+  defer ln.Close()
+  
+  conexao, erro2 := ln.Accept()
+  if erro2 != nil {
+    fmt.Println(erro2)
+    os.Exit(3)
+  }
+
+  defer conexao.Close() 
+
+  fmt.Println("Conexão aceita...")
+
+  for {
+    mensagem, erro3 := bufio.NewReader(conexao).ReadString('\n')
+    if erro3 != nil {
+      fmt.Println(erro3)
+      os.Exit(3)
+    }
+
+    
+    fmt.Print("Mensagem recebida:", string(mensagem))
+
+    novamensagem := strings.ToUpper(mensagem)
+
+    conexao.Write([]byte(novamensagem + "\n"))
+  }
+}
